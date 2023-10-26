39b000f8597347e621287b2a7017f6636463a3ae
aryawardhana <wardhana2451@gmail.com>
2023-10-26 23:33:30 +0700

    first push

---
+++ view/layouts/footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+        <div class="info-main">
+            <p><strong>What is Astuff </strong><br>
+            Astuff is a marketplace for buying-selling item arts.<br>
+            In Astuff you can connect your self with the buyer in realtime.
+            </p>
+        </div>
+        <div class="info-work">
+            <p><strong>How does Astuff work</strong><br>
+            In Astuff you can use many cryptocurancy as a paiment. <br>
+            You can press learn more if you want more information about this site
+            </p>
+        </div>
+        <button class="btn btn-outline-light">Learn More</button>
+    </div>
---
+++ view/layouts/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+    <img src="./public/img/aStuff.png" alt="kopi" class="logo">
+    <ul>
+        <li><a href="features">Features</a></li>
+        <li><a href="marketpalce">Market</a></li>
+        <li><a href="community">Community</a></li>
+        <a href="#"><span class="material-symbols-outlined" >
+            person
+        </span>
+        </a>
+    </ul>
+
+ 
+</nav>
