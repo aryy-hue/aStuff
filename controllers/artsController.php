a1f75bf071f459b54ab6e742a0a4592e7f6cb987
aryawardhana <wardhana2451@gmail.com>
2023-10-28 00:48:41 +0700

    adding add-arts feature

--- controllers/artsController.php
+++ controllers/artsController.php
@@ -6,11 +6,27 @@ include('../models/dataBaseConn.php');
 $art_name = $_POST['art_name'];
 $date_added = $_POST['date_added'];
 $name = $_POST['name'];
-$img = $_POST['img'];
+$price = $_POST['price'];
 
-// menginput data ke database
-$query = "INSERT INTO arts (art_name, date_added, name, img) VALUES ('$art_name', '$date_added', '$name', '$img')";
+$rand = rand();
+$ekstensi =  array('png','jpg','jpeg','gif','webp');
+$filename = $_FILES['img']['name'];
+$ukuran = $_FILES['img']['size'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+$maxFileSize = 20 * 1024 * 1024; 
 
+if(!in_array($ext,$ekstensi) ) {
+	header("location:index.php?alert=gagal_ekstensi");
+}else{
+	if($ukuran < $maxFileSize){		
+		$img = $rand.'_'.$filename;
+		move_uploaded_file($_FILES['img']['tmp_name'], '../public/uploads/'.$rand.'_'.$filename);
+		mysqli_query($koneksi, "INSERT INTO arts VALUES(' ','$art_name', '$date_added',' ','$name', '$img' , '$price')");
+		header("location:/aStuff/index.php");
+	}else{
+		header("location:/aStuff/index.php?alert=gagal_ukuran");
+	}
+}
 if(mysqli_query($koneksi, $query)) {
     // Data inserted successfully, redirect to tampil.php
     header("location: /aStuff/index.php");
--- view/pages/market.php
+++ view/pages/market.php
@@ -9,77 +9,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-<!doctype html>
     <div class="hero">
         <?php include('../layouts/navbar.php'); ?>
     </div>
+   
     <div class="container">
+        <?php include('../../models/dataBaseConn.php');?>
+        <?php
+            $no = 1;
+            $data = mysqli_query($koneksi,"select * from arts");
+            while($d = mysqli_fetch_array($data)){
+        ?>
         <div class="container-market">
             <div class="marketplace">
                 <div class="market-img">
-                    <img src="https://media0.giphy.com/media/gw3Pz3YQZREedZbG/giphy.gif?cid=ecf05e47gsybd0tadyj0jhuk1oz3t0b42irw39beb4qvbxb2&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="highlight">
+                    <img src="../../public/uploads/<?php echo $d['img'] ?>" alt="highlight">
                 </div>
                 <div class="market-info">
                     <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
+                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@ <?php echo $d['name']; ?></p>
+                    <p class="price"><strong>Price</strong><br> + <?php echo $d['price']; ?></p>
                 </div>
             </div>
-            
-            <div class="marketplace">
-                <div class="market-img">
-                    <img src="https://i.pinimg.com/originals/07/85/a4/0785a4d12b21d25e962fc0cc1a6f145a.gif" alt="highlight">
-                </div>
-                <div class="market-info">
-                    <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
-                </div>
-            </div>
-            <div class="marketplace">
-                <div class="market-img">
-                    <img src="https://i.pinimg.com/originals/07/85/a4/0785a4d12b21d25e962fc0cc1a6f145a.gif" alt="highlight">
-                </div>
-                <div class="market-info">
-                    <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
-                </div>
-            </div>
-            <div class="marketplace">
-                <div class="market-img">
-                    <img src="https://news.artnet.com/app/news-upload/2021/02/NYAN-CAT-ARTINTERVIEW-copy.jpg" alt="highlight">
-                </div>
-                <div class="market-info">
-                    <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
-                </div>
-            </div>
-            <div class="marketplace">
-                <div class="market-img">
-                    <img src="https://images.squarespace-cdn.com/content/v1/604a3b7f8195a274403f26be/1676475834891-4OLGC99391KD99TPY456/imposter+%285%29.gif?format=500w" alt="highlight">
-                </div>
-                <div class="market-info">
-                    <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
-                </div>
-            </div>
-            <div class="marketplace">
-                <div class="market-img">
-                    <img src="https://images.squarespace-cdn.com/content/v1/604a3b7f8195a274403f26be/1676475834891-4OLGC99391KD99TPY456/imposter+%285%29.gif?format=500w" alt="highlight">
-                </div>
-                <div class="market-info">
-                    <p>
-                        <img src="/aStuff/public/img/artist.jpg" alt="Artist" class="artist">@asep</p>
-                    <p class="price"><strong>Price</strong><br> $1+</p>
-                </div>
-            </div>
-            
         </div>
+        <?php }?>
     </div>
-    <?php include('../layouts/footer.php'); ?>
+    <?php include('../layouts/footer.php'); ?>   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
